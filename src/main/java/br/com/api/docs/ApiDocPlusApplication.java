dbb5b488ac1b007d96c5822de61644cba1af8c09
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-09-26 19:14:48 -0300

    initial commit

---
+++ src/main/java/br/com/api/docs/ApiDocPlusApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDocPlusApplication.class, args);
+	}
+
+}
