61922a787fb0900a08a40dde72165cddfeec24f4
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-09-27 15:00:08 -0300

    feat: mapeia para download response dto

---
+++ src/main/java/br/com/api/docs/mapper/UserDocMapper.java
@@ -0,0 +1,30 @@
+package br.com.api.docs.mapper;
+
+import br.com.api.docs.domain.entities.UserDoc;
+import br.com.api.docs.dto.userdoc.UserDocDownloadResponseDTO;
+import br.com.api.docs.dto.userdoc.UserDocResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDocMapper {
+
+    public UserDocResponseDTO mapEntityToUserDocResponse(UserDoc userDoc, String accessUrl) {
+        return UserDocResponseDTO.builder()
+                .id(userDoc.getId())
+                .userId(userDoc.getUserId())
+                .categoryId(userDoc.getCategoryId())
+                .documentName(userDoc.getDocumentName())
+                .documentType(userDoc.getDocumentType())
+                .fileUrl(userDoc.getFileUrl())
+                .accessUrl(accessUrl)
+                .build();
+    }
+
+    public UserDocDownloadResponseDTO mapToUserDocDownloadResponse(byte[] content, String documentName, String documentType) {
+        return UserDocDownloadResponseDTO.builder()
+                .content(content)
+                .documentName(documentName)
+                .documentType(documentType)
+                .build();
+    }
+}
