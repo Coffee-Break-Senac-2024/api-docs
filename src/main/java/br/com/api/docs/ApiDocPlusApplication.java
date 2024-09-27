6d2edc1272440cb500c0eb6cc02e2ca62e145a78
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-09-27 02:44:25 -0300

    enabla feign

--- src/main/java/br/com/api/docs/ApiDocPlusApplication.java
+++ src/main/java/br/com/api/docs/ApiDocPlusApplication.java
@@ -2,8 +2,10 @@ package br.com.api.docs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ApiDocPlusApplication {
 
 	public static void main(String[] args) {
