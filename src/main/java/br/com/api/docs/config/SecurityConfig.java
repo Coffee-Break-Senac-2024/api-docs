e7e48b40e70426e2a44a0f5f5d004fb252979bc5
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-11-02 16:18:50 -0300

    fix cors

--- src/main/java/br/com/api/docs/config/SecurityConfig.java
+++ src/main/java/br/com/api/docs/config/SecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,7 +19,16 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.csrf(csrf -> csrf.disable()).cors(cors -> {
+            cors.configurationSource(request -> {
+                var configuration = new CorsConfiguration();
+                configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+                configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+                configuration.setAllowCredentials(true);
+                return configuration;
+            });
+        });
         httpSecurity.addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
         return httpSecurity.build();
     }
